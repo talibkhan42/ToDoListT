2780c3ec4fed2a3c083d49d0529fa7eb12efff4c
talibkhan <talibrazakhan146@gmail.com>
2022-08-29 11:24:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css"
+export default function App() {
+  const [Input, setInput] = useState("");
+  const [Item, setItem] = useState([]);
+  const [buttonText, setButtonText] = useState("Add");
+  const [updateItemId, setupdateItemId] = useState(-1);
 
-function App() {
+  const HandlerChangeInput = (e) => {
+    setInput(e.target.value);
+  };
+  const HandlerAddItem = () => {
+    if(updateItemId>=0){
+        const data=[Item];
+        data[0][updateItemId]=Input;
+        setItem(data[0])
+        setupdateItemId(-1);
+        setButtonText("Add");
+        setInput("");
+    }else
+    {
+        if(!Input){
+            alert(" oops please enter item")
+          }else{
+            setItem([...Item, Input]);
+            setInput('')
+          }
+    }
+  };
+
+  const HandlerDeleteItem=(id)=>{
+    console.log("delete")
+
+  const UpdatedItem=Item.filter((ele,ind)=>{
+    return ind !== id
+  })
+  setItem(UpdatedItem);
+
+  }
+  const HandlerEditItem=(id)=>{
+    setButtonText("update");
+    setupdateItemId(id);
+    setInput(Item[id]);
+    console.log(id);
+
+  }
+
+  const HandlerRemoveAllItem=()=>{
+    setItem([]);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="App-header">
+
+      <div >
+      <h1>To <span className="span"> Do </span>List</h1>
+      <div >
+
+        <input className="input"
+          type="text"
+          placeholder="enter any item"
+          onChange={HandlerChangeInput}
+          value={Input}
+        ></input>
+        <button className="add" onClick={HandlerAddItem}>{buttonText}</button>
+      </div>
+        
+      </div>
+      <ol>
+        {Item.map((Element, index) => {
+          return (
+            <div key={index} >
+            <div className="list">
+              <li>{Element} <button className="delete" onClick={()=>{HandlerDeleteItem(index)}}>❎</button>
+               <button className="edit" onClick={()=>{HandlerEditItem(index)}} >Edit</button></li>
+              
+            </div>
+            <br></br>
+            </div>
+          );
+        })}
+        
+      </ol>
+      <button onClick={HandlerRemoveAllItem}>Remove All Items</button>
     </div>
+
+    </>
   );
 }
-
-export default App;
